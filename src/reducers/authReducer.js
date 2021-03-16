ef1d4e59f881e7b21f07b37a0aae95881eea7e19
Lucas Alves Nepomucena <lucas.nepomucena@infracommerce.com.br>
2021-03-16 00:28:31 -0300

    fix(main): remove warning eslint

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 const INITIAL_STATE = {
   isSignedIn: null,
 };
-
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "SIGN_IN":
