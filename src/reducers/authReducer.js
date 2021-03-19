43db8cf3bd6e0e210502da083e330b6b7b6c8cdc
Lucas Alves Nepomucena <lucas.nepomucena@infracommerce.com.br>
2021-03-19 00:03:00 -0300

    refactor(main): add types

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,12 +1,15 @@
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
+
 const INITIAL_STATE = {
   isSignedIn: null,
+  userId: null,
 };
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SIGN_IN":
+    case SIGN_IN:
       return { ...state, isSignedIn: true };
-    case "SIGN_OUT":
+    case SIGN_OUT:
       return { ...state, isSignedIn: false };
     default:
       return state;
